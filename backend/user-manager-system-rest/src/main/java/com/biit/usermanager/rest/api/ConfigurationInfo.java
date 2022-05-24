f078973b5d0019bdd8a65d1744aacb6407040566
Jorge Hortelano <jhortelano@biit-solutions.com>
2022-05-24 18:12:55 +0200

    Swagger moved to rest server project

--- backend/user-manager-system-rest/src/main/java/com/biit/usermanager/rest/api/ConfigurationInfo.java
+++ backend/user-manager-system-rest/src/main/java/com/biit/usermanager/rest/api/ConfigurationInfo.java
@@ -15,26 +15,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-public class Info {
+@RequestMapping("/info")
+public class ConfigurationInfo {
 
     private Environment environment;
     private ApplicationArguments applicationArguments;
 
     @Autowired
-    public Info(Environment environment, ApplicationArguments applicationArguments) {
+    public ConfigurationInfo(Environment environment, ApplicationArguments applicationArguments) {
         this.environment = environment;
         this.applicationArguments = applicationArguments;
     }
 
-    @Operation(summary = "Basic method to check if the server is online.")
-    @RequestMapping(value = "/health-check", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public void healthCheck(HttpServletRequest httpRequest) {
-
-    }
-
     @Operation(summary = "Configuration info")
-    @PostMapping(value = "/info", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.TEXT_PLAIN_VALUE)
+    @PostMapping(value = "/configuration", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.TEXT_PLAIN_VALUE)
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public Map<String, String> info(HttpServletRequest request, @RequestBody String hash)
@@ -59,7 +53,7 @@ public class Info {
         info.put("server.port", environment.getProperty("server.port"));
         info.put("server.servlet.context-path", environment.getProperty("server.servlet.context-path"));
         info.put("environment", environment.getProperty("environment"));
-        info.put("path", Info.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+        info.put("path", ConfigurationInfo.class.getProtectionDomain().getCodeSource().getLocation().getPath());
         for (final String arg : applicationArguments.getOptionNames()) {
             info.put("application.args." + arg, applicationArguments.getOptionValues(arg).get(0));
         }
