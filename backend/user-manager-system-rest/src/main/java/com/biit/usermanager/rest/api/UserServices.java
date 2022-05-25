ac8a1ca72253e9b0f5b88f0a52cf2029cb73f448
Jorge Hortelano <jhortelano@biit-solutions.com>
2022-05-25 16:59:22 +0200

    Basic user services.

---
+++ backend/user-manager-system-rest/src/main/java/com/biit/usermanager/rest/api/UserServices.java
@@ -0,0 +1,78 @@
+package com.biit.usermanager.rest.api;
+
+import com.biit.usermanager.core.controller.UserController;
+import com.biit.usermanager.core.controller.models.UserDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserServices {
+    private final UserController userController;
+
+    public UserServices(UserController userController) {
+        this.userController = userController;
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER_MANAGER_VIEWER')")
+    @Operation(summary = "Gets all users.", security = @SecurityRequirement(name = "bearerAuth"))
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<UserDTO> getAll(HttpServletRequest request) {
+        return userController.get();
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER_MANAGER_VIEWER')")
+    @Operation(summary = "Counts all users.", security = @SecurityRequirement(name = "bearerAuth"))
+    @GetMapping(value = "/count", produces = MediaType.APPLICATION_JSON_VALUE)
+    public long count(HttpServletRequest request) {
+        return userController.count();
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER_MANAGER_VIEWER')")
+    @Operation(summary = "Gets a user.", security = @SecurityRequirement(name = "bearerAuth"))
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public UserDTO get(@Parameter(description = "Id of an existing user", required = true) @PathVariable("id") Long id,
+                       HttpServletRequest request) {
+        return userController.get(id);
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER_MANAGER_ADMIN')")
+    @Operation(summary = "Creates a user.", security = @SecurityRequirement(name = "bearerAuth"))
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public UserDTO add(@RequestBody UserDTO userDTO, HttpServletRequest request) {
+        return userController.create(userDTO);
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER_MANAGER_ADMIN')")
+    @Operation(summary = "Deletes a user.", security = @SecurityRequirement(name = "bearerAuth"))
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void delete(@Parameter(description = "Id of an existing user", required = true) @PathVariable("id") Long id,
+                       HttpServletRequest request) {
+        userController.deleteById(id);
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER_MANAGER_ADMIN')")
+    @Operation(summary = "Deletes a user.", security = @SecurityRequirement(name = "bearerAuth"))
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PostMapping(value = "/delete", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void delete(@RequestBody UserDTO userDTO, HttpServletRequest request) {
+        userController.delete(userDTO);
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER_MANAGER_ADMIN')")
+    @Operation(summary = "Updates a user.", security = @SecurityRequirement(name = "bearerAuth"))
+    @PutMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public UserDTO update(@RequestBody UserDTO userDTO, HttpServletRequest request) {
+        return userController.update(userDTO);
+    }
+}
