7c0a309aa6953f07df8a48a422698e22fb160abb
Mariano <mgarcia@biit-solutions.es>
2022-09-15 17:34:51 +0200

    Fixed merge

--- backend/user-manager-system-persistence/src/test/java/com/biit/usermanager/persistence/repositories/UserNotDisableTest.java
+++ backend/user-manager-system-persistence/src/test/java/com/biit/usermanager/persistence/repositories/UserNotDisableTest.java
@@ -5,8 +5,10 @@ import com.biit.usermanager.persistence.entities.User;
 import com.biit.usermanager.persistence.entities.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
 import java.util.Date;
@@ -15,21 +17,17 @@ import java.util.Optional;
 
 @SpringBootTest
 @Test(groups = {"userRepository"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class UserNotDisableTest extends AbstractTestNGSpringContextTests {
-
-
-    String USER_NAME_1 = "Ana";
-    String USER_NAME_2 = "Adri";
-    String USER_NAME_3 = "Mariano";
-
-    Boolean enable = true;
+    private final static String USER_NAME_1 = "Ana";
+    private final static String USER_NAME_2 = "Adri";
+    private final static String USER_NAME_3 = "Mariano";
 
     @Autowired
     UserRepository userRepository;
 
     @Test
     public void saveUser() {
-
         User user1 = new User();
         User user2 = new User();
         User user3 = new User();
@@ -53,7 +51,6 @@ public class UserNotDisableTest extends AbstractTestNGSpringContextTests {
         Assert.assertNotNull(user1.getId());
         Assert.assertNotNull(user2.getId());
         Assert.assertNotNull(user3.getId());
-
     }
 
     @Test(dependsOnMethods = "saveUser")
@@ -63,5 +60,11 @@ public class UserNotDisableTest extends AbstractTestNGSpringContextTests {
         for (User user : usersList) {
             Assert.assertTrue(user.isEnabled());
         }
+    }
+
+    @AfterClass
+    public void cleanUpUsers(){
+        userRepository.deleteAll();
+        Assert.assertEquals(userRepository.count(), 0);
     }
 }
--- backend/user-manager-system-persistence/src/test/java/com/biit/usermanager/persistence/repositories/UserRepositoryTests.java
+++ backend/user-manager-system-persistence/src/test/java/com/biit/usermanager/persistence/repositories/UserRepositoryTests.java
@@ -3,8 +3,10 @@ package com.biit.usermanager.persistence.repositories;
 import com.biit.usermanager.persistence.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -12,6 +14,7 @@ import java.util.Optional;
 
 @SpringBootTest
 @Test(groups = {"userRepository"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class UserRepositoryTests extends AbstractTestNGSpringContextTests {
     private static String USER_NAME = "TestUser";
     private static String PHONE = "902202122";
@@ -68,5 +71,11 @@ public class UserRepositoryTests extends AbstractTestNGSpringContextTests {
             Assert.assertTrue(user.isAccountExpired());
         }
         Assert.assertEquals(users.size(), 1);
+    }
+
+    @AfterClass
+    public void cleanUpUsers(){
+        userRepository.deleteAll();
+        Assert.assertEquals(userRepository.count(), 0);
     }
 }
