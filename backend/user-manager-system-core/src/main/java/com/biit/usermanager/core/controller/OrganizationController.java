b06a24b12e72002b2f7eee1810ef2de3ce97b089
Agonzalez <agonzalez@biit-solutions.es>
2022-09-27 17:26:35 +0200

    ServiceUser, testUser and testOrganization

--- backend/user-manager-system-core/src/main/java/com/biit/usermanager/core/controller/OrganizationController.java
+++ backend/user-manager-system-core/src/main/java/com/biit/usermanager/core/controller/OrganizationController.java
@@ -4,11 +4,16 @@ package com.biit.usermanager.core.controller;
 import com.biit.usermanager.core.controller.models.OrganizationDTO;
 import com.biit.usermanager.core.converters.OrganizationConverter;
 import com.biit.usermanager.core.converters.models.OrganizationConverterRequest;
+import com.biit.usermanager.core.converters.models.UserConverterRequest;
+import com.biit.usermanager.core.exceptions.UserNotFoundException;
 import com.biit.usermanager.core.providers.OrganizationProvider;
 import com.biit.usermanager.persistence.entities.Organization;
 import com.biit.usermanager.persistence.repositories.OrganizationRepository;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import java.util.Optional;
 
 @Controller
 public class OrganizationController extends BasicInsertableController<Organization, OrganizationDTO, OrganizationRepository,
@@ -23,4 +28,9 @@ public class OrganizationController extends BasicInsertableController<Organizati
     protected OrganizationConverterRequest createConverterRequest(Organization entity) {
         return new OrganizationConverterRequest(entity);
     }
+
+    public OrganizationDTO getByName(String name) {
+        return converter.convert(new OrganizationConverterRequest(provider.findByName(name).orElseThrow(() -> new UserNotFoundException(this.getClass(),
+                "No User with username '" + name + "' found on the system."))));}
+
 }
