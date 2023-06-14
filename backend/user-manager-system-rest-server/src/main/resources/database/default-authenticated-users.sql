699a1f0ccc93cd300a57f8371fd91ba869adab98
Jorge Hortelano <jhortelano@biit-solutions.com>
2023-06-14 17:39:11 +0200

    Trying to fix swagger https redirects to http

--- backend/user-manager-system-rest-server/src/main/resources/database/default-authenticated-users.sql
+++ backend/user-manager-system-rest-server/src/main/resources/database/default-authenticated-users.sql
@@ -1,9 +1,9 @@
-INSERT IGNORE INTO users (id, username, email, name, lastname, password, phone, account_locked, account_blocked, account_expired, enabled) VALUES (-1, 'admin@test.com', 'admin@test.com', 'Admin', 'Admin', '$2a$12$wU5OFo.IvHq7xfoHtCqp0.yzoENB.iunHuFJD6ktM9ygm3Zxbx9fq', '+34722177548', 0, 0, 0, 1);
-INSERT IGNORE INTO applications (id, name) VALUES (-5, 'UserManagerSystem');
-INSERT IGNORE INTO roles (id, name, description) VALUES (-2, 'usermanagersystem_admin', '');
-INSERT IGNORE INTO roles (id, name, description) VALUES (-3, 'usermanagersystem_viewer', '');
-INSERT IGNORE INTO groups (id, name) VALUES (-4, 'localhost');
-INSERT IGNORE INTO user_roles (id, user_group, role, user_role, `application`) VALUES (-6, -4 , -2, -1, -5);
-INSERT IGNORE INTO user_roles (id, user_group, role, user_role, `application`) VALUES (-7, -4 , -3, -1, -5);
-INSERT IGNORE INTO user_roles (id, user_group, role, user_role, `application`) VALUES (-8, null, -2, -1, null);
-INSERT IGNORE INTO user_roles (id, user_group, role, user_role, `application`) VALUES (-9, null, -3, -1, null);
+INSERT INTO users (id, username, email, name, lastname, password, phone, account_locked, account_blocked, account_expired, enabled) VALUES (-1, 'admin@test.com', 'admin@test.com', 'Admin', 'Admin', '$2a$12$wU5OFo.IvHq7xfoHtCqp0.yzoENB.iunHuFJD6ktM9ygm3Zxbx9fq', '+34722177548', 0, 0, 0, 1);
+INSERT INTO applications (id, name) VALUES (-5, 'UserManagerSystem');
+INSERT INTO roles (id, name, description) VALUES (-2, 'usermanagersystem_admin', '');
+INSERT INTO roles (id, name, description) VALUES (-3, 'usermanagersystem_viewer', '');
+INSERT INTO groups (id, name) VALUES (-4, 'localhost');
+INSERT INTO user_roles (id, user_group, role, user_role, application) VALUES (-6, -4 , -2, -1, -5);
+INSERT INTO user_roles (id, user_group, role, user_role, application) VALUES (-7, -4 , -3, -1, -5);
+INSERT INTO user_roles (id, user_group, role, user_role, application) VALUES (-8, null, -2, -1, null);
+INSERT INTO user_roles (id, user_group, role, user_role, application) VALUES (-9, null, -3, -1, null);
