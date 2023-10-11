247e7e651d0c108823a6c3e17061938079c5ede0
Jorge Hortelano <jhortelano@biit-solutions.com>
2023-10-11 15:53:21 +0200

    Adding default customer users

--- backend/user-manager-system-rest-server/src/main/resources/database/default-authenticated-users.sql
+++ backend/user-manager-system-rest-server/src/main/resources/database/default-authenticated-users.sql
@@ -21,6 +21,7 @@ INSERT INTO user_roles (id, user_group, role, user_role) VALUES (-26, -24 , -22,
 INSERT INTO user_roles (id, user_group, role, user_role) VALUES (-27, -24 , -23, -1);
 
 
+
 -- Task Junction
 INSERT INTO applications (id, name) VALUES (-30, 'TaskJunction');
 INSERT INTO roles (id, name, description) VALUES (-31, 'taskjunction_viewer', '');
@@ -52,6 +53,7 @@ INSERT INTO groups (id, name, application_id) VALUES (-54, 'users', -50);
 INSERT INTO user_roles (id, user_group, role, user_role) VALUES (-55, -54 , -51, -1);
 INSERT INTO user_roles (id, user_group, role, user_role) VALUES (-56, -54 , -52, -1);
 INSERT INTO user_roles (id, user_group, role, user_role) VALUES (-57, -54 , -53, -1);
+
 
 -- Base Form Drools Engine
 INSERT INTO applications (id, name) VALUES (-60, 'BaseFormDroolsEngine');
@@ -93,3 +95,26 @@ INSERT INTO groups (id, name, application_id) VALUES (-94, 'users', -90);
 INSERT INTO user_roles (id, user_group, role, user_role) VALUES (-95, -94 , -91, -1);
 INSERT INTO user_roles (id, user_group, role, user_role) VALUES (-96, -94 , -92, -1);
 INSERT INTO user_roles (id, user_group, role, user_role) VALUES (-97, -94 , -93, -1);
+
+
+-- Demo Users
+INSERT INTO users (id, user_uuid, username, email, name, lastname, password, phone, account_locked, account_blocked, account_expired) VALUES (-150, '15fca0dd-dd89-41dd-9448-6deb4211c204', 'customer1@test.com', 'customer1@test.com', 'Tom', 'Smith', '$2a$10$V84FYKhHogkULl5OlAzw1.mLquRv0Y5kAOtYgUBnv0NuE56KGlBnO', '+345551234', 0, 0, 0);
+INSERT INTO users (id, user_uuid, username, email, name, lastname, password, phone, account_locked, account_blocked, account_expired) VALUES (-151, 'aa727351-210b-43f7-bc16-6d023ddf31b5', 'customer2@test.com', 'customer2@test.com', 'Dick', 'Taylor', '$2a$10$V84FYKhHogkULl5OlAzw1.mLquRv0Y5kAOtYgUBnv0NuE56KGlBnO', '+345552234', 0, 0, 0);
+INSERT INTO users (id, user_uuid, username, email, name, lastname, password, phone, account_locked, account_blocked, account_expired) VALUES (-152, '87a580f7-4449-4b97-baee-c9f27b65ce4b', 'customer3@test.com', 'customer3@test.com', 'Harry', 'Evans', '$2a$10$V84FYKhHogkULl5OlAzw1.mLquRv0Y5kAOtYgUBnv0NuE56KGlBnO', '+345553234', 0, 0, 0);
+INSERT INTO users (id, user_uuid, username, email, name, lastname, password, phone, account_locked, account_blocked, account_expired) VALUES (-153, 'd9166f80-28af-4c2b-a34a-e5b3ee37f5aa', 'customer4@test.com', 'customer4@test.com', 'Tizio', 'Russo', '$2a$10$V84FYKhHogkULl5OlAzw1.mLquRv0Y5kAOtYgUBnv0NuE56KGlBnO', '+345554234', 0, 0, 0);
+INSERT INTO users (id, user_uuid, username, email, name, lastname, password, phone, account_locked, account_blocked, account_expired) VALUES (-154, '46cba6a8-f2ab-4d18-a6c3-5f1a1bcae731', 'customer5@test.com', 'customer5@test.com', 'Caio', 'Fabbro', '$2a$10$V84FYKhHogkULl5OlAzw1.mLquRv0Y5kAOtYgUBnv0NuE56KGlBnO', '+345555234', 0, 0, 0);
+INSERT INTO users (id, user_uuid, username, email, name, lastname, password, phone, account_locked, account_blocked, account_expired) VALUES (-155, '47d251cf-4238-4582-b6cc-18062eacc2e8', 'customer6@test.com', 'customer6@test.com', 'Sempronio', 'Colombo', '$2a$10$V84FYKhHogkULl5OlAzw1.mLquRv0Y5kAOtYgUBnv0NuE56KGlBnO', '+345556234', 0, 0, 0);
+-- Kafka Proxy Permissions
+INSERT INTO user_roles (id, user_group, role, user_role) VALUES (-170, -24 , -22, -150);
+INSERT INTO user_roles (id, user_group, role, user_role) VALUES (-171, -24 , -22, -151);
+INSERT INTO user_roles (id, user_group, role, user_role) VALUES (-172, -24 , -22, -152);
+INSERT INTO user_roles (id, user_group, role, user_role) VALUES (-173, -24 , -22, -153);
+INSERT INTO user_roles (id, user_group, role, user_role) VALUES (-174, -24 , -22, -154);
+INSERT INTO user_roles (id, user_group, role, user_role) VALUES (-175, -24 , -22, -155);
+-- Fact Manager Permissions
+INSERT INTO user_roles (id, user_group, role, user_role) VALUES (-190, -54 , -51, -150);
+INSERT INTO user_roles (id, user_group, role, user_role) VALUES (-191, -54 , -51, -151);
+INSERT INTO user_roles (id, user_group, role, user_role) VALUES (-192, -54 , -51, -152);
+INSERT INTO user_roles (id, user_group, role, user_role) VALUES (-193, -54 , -51, -153);
+INSERT INTO user_roles (id, user_group, role, user_role) VALUES (-194, -54 , -51, -154);
+INSERT INTO user_roles (id, user_group, role, user_role) VALUES (-195, -54 , -51, -155);
