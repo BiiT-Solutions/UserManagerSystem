88d4cd5442b6ab4a96792bc22591046fdd1383dd
Jorge Hortelano <jhortelano@biit-solutions.com>
2023-11-28 12:35:10 +0100

    Removed group definition from Kafka

--- user-manager-system-core/src/main/java/com/biit/usermanager/core/kafka/EventConsumerListener.java
+++ user-manager-system-core/src/main/java/com/biit/usermanager/core/kafka/EventConsumerListener.java
@@ -2,7 +2,6 @@ package com.biit.usermanager.core.kafka;
 
 import com.biit.kafka.consumers.EventListener;
 import com.biit.kafka.events.Event;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -13,20 +12,23 @@ import org.springframework.messaging.handler.annotation.Payload;
 /***
  * Reads any event on a specific topic. Later will call the Controller to handle the event.
  */
-@ConditionalOnExpression("${spring.kafka.enabled:false}")
+/*@ConditionalOnExpression("${spring.kafka.enabled:false}")*/
 @EnableKafka
 @Configuration
 public class EventConsumerListener extends EventListener {
 
     @Override
-    @KafkaListener(topics = "${spring.kafka.topic:#{null}}", clientIdPrefix = "firstListener",
+    @KafkaListener(topics = "${spring.kafka.topic:#{null}}",
+            clientIdPrefix = "#{${spring.kafka.client.id}?:T(java.util.UUID).randomUUID().toString()}",
+            groupId = "#{${spring.kafka.group.id}?:T(java.util.UUID).randomUUID().toString()}",
             containerFactory = "templateEventListenerContainerFactory", autoStartup = "${spring.kafka.enabled:true}")
     public void eventsListener(@Payload(required = false) Event event,
                                final @Header(KafkaHeaders.OFFSET) Integer offset,
+                               final @Header(KafkaHeaders.GROUP_ID) String groupId,
                                final @Header(value = KafkaHeaders.KEY, required = false) String key,
                                final @Header(KafkaHeaders.RECEIVED_PARTITION) int partition,
                                final @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
                                final @Header(KafkaHeaders.RECEIVED_TIMESTAMP) long timeStamp) {
-        super.eventsListener(event, offset, key, partition, topic, timeStamp);
+        super.eventsListener(event, offset, groupId, key, partition, topic, timeStamp);
     }
 }
