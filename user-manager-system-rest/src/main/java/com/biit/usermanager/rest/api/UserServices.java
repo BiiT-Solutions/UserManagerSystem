b08a0412c38fc4ab6af7ed78e6028f421f56fe0e
Jorge Hortelano <jhortelano@biit-solutions.com>
2024-05-06 12:00:15 +0200

    Corrected endpoint

--- user-manager-system-rest/src/main/java/com/biit/usermanager/rest/api/UserServices.java
+++ user-manager-system-rest/src/main/java/com/biit/usermanager/rest/api/UserServices.java
@@ -305,7 +305,7 @@ public class UserServices extends ElementServices<User, Long, UserDTO, UserRepos
 
     @PreAuthorize("hasAnyAuthority(@securityService.adminPrivilege, @securityService.editorPrivilege, @securityService.viewerPrivilege)")
     @Operation(summary = "Get Organization's users", security = @SecurityRequirement(name = "bearerAuth"))
-    @GetMapping(value = "/organanizations/{organizationName}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/organizations/{organizationName}", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<UserDTO> getUsersByOrganization(@Parameter(description = "Name of an existing organization", required = true) @PathVariable("organizationName")
                                                 String organizationName,
                                                 HttpServletRequest request) {
