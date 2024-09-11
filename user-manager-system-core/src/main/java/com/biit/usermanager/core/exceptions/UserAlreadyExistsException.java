4fa93da32840ebf1a228664489ffb2a5231e7359
Jorge Hortelano <jhortelano@biit-solutions.com>
2024-09-11 11:31:50 +0200

    Update exceptions to include frotend codes

--- user-manager-system-core/src/main/java/com/biit/usermanager/core/exceptions/UserAlreadyExistsException.java
+++ user-manager-system-core/src/main/java/com/biit/usermanager/core/exceptions/UserAlreadyExistsException.java
@@ -1,12 +1,12 @@
 package com.biit.usermanager.core.exceptions;
 
 import com.biit.logger.ExceptionType;
-import com.biit.server.exceptions.NotFoundException;
+import com.biit.server.logger.LoggedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class UserAlreadyExistsException extends NotFoundException {
+public class UserAlreadyExistsException extends LoggedException {
     private static final long serialVersionUID = 7032994111678894370L;
 
     public UserAlreadyExistsException(Class<?> clazz, String message, ExceptionType type) {
@@ -14,11 +14,11 @@ public class UserAlreadyExistsException extends NotFoundException {
     }
 
     public UserAlreadyExistsException(Class<?> clazz, String message) {
-        super(clazz, message);
+        super(clazz, message, ExceptionType.INFO, HttpStatus.BAD_REQUEST);
     }
 
     public UserAlreadyExistsException(Class<?> clazz) {
-        this(clazz, "User not found");
+        this(clazz, "User already exists!");
     }
 
     public UserAlreadyExistsException(Class<?> clazz, Throwable e) {
--- user-manager-system-rest/src/main/java/com/biit/usermanager/rest/exceptions/UserManagerExceptionControllerAdvice.java
+++ user-manager-system-rest/src/main/java/com/biit/usermanager/rest/exceptions/UserManagerExceptionControllerAdvice.java
@@ -12,7 +12,7 @@ import com.biit.usermanager.core.exceptions.TeamAlreadyExistsException;
 import com.biit.usermanager.core.exceptions.TokenExpiredException;
 import com.biit.usermanager.core.exceptions.UserAlreadyExistsException;
 import org.apache.kafka.common.errors.InvalidRequestException;
-import org.modelmapper.spi.ErrorMessage;
+import com.biit.server.exceptions.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -24,64 +24,65 @@ public class UserManagerExceptionControllerAdvice extends ServerExceptionControl
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<Object> notFoundException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage(ex.getMessage(), ex), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "not_found", ex), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(InvalidRequestException.class)
     public ResponseEntity<Object> invalidRequestException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage(ex.getMessage()), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "invalid_request", ex), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<Object> userAlreadyExistsException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage(ex.getMessage(), ex), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "user_already_exists", ex), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidPasswordException.class)
     public ResponseEntity<Object> invalidPasswordException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage(ex.getMessage()), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "invalid_password", ex), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(RoleWithoutBackendServiceRoleException.class)
     public ResponseEntity<Object> roleWithoutBackendServiceRoleException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage("Role has no linked backend services. Backend services are necessary on each role."),
+        return new ResponseEntity<>(new ErrorResponse("Role has no linked backend services. Backend services are necessary on each role.",
+                "role_without_backend_service_role"),
                 HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(OrganizationAlreadyExistsException.class)
     public ResponseEntity<Object> organizationAlreadyExistsException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage("Organization already exists. ", ex),
+        return new ResponseEntity<>(new ErrorResponse("Organization already exists.", "organization_already_exists", ex),
                 HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(TeamAlreadyExistsException.class)
     public ResponseEntity<Object> teamAlreadyExistsException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage("Team already exists. ", ex),
+        return new ResponseEntity<>(new ErrorResponse("Team already exists.", "team_already_exists", ex),
                 HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(TokenExpiredException.class)
     public ResponseEntity<Object> tokenExpiredException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage(ex.getMessage()), HttpStatus.FORBIDDEN);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "token_expired"), HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(ApplicationBackendRoleNotFoundException.class)
     public ResponseEntity<Object> applicationBackendRoleNotFoundException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage(ex.getMessage()), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "application_backend_role_not_found"), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(EmailNotSentException.class)
     public ResponseEntity<Object> emailNotSentException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage(ex.getMessage()), HttpStatus.SERVICE_UNAVAILABLE);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "email_not_sent"), HttpStatus.SERVICE_UNAVAILABLE);
     }
 
 
