a331e80e3915c953600595771e333909b657a9e0
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-04-03 15:12:17 +0200

    Fixing checkstyle

--- user-manager-system-rest/src/main/java/com/biit/usermanager/rest/api/UserServices.java
+++ user-manager-system-rest/src/main/java/com/biit/usermanager/rest/api/UserServices.java
@@ -46,7 +46,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.security.SecureRandom;
 import java.util.Collection;
 import java.util.List;
-import java.util.Random;
 import java.util.UUID;
 
 @RestController
