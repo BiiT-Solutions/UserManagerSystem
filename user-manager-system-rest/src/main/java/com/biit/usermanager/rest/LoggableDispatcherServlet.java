e2f4d7d36ad66035681e151030672af93b93ceb2
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-08-26 12:35:12 +0200

    Catching exception

--- user-manager-system-rest/src/main/java/com/biit/usermanager/rest/LoggableDispatcherServlet.java
+++ user-manager-system-rest/src/main/java/com/biit/usermanager/rest/LoggableDispatcherServlet.java
@@ -1,19 +1,27 @@
 package com.biit.usermanager.rest;
 
+import com.biit.usermanager.logger.UserManagerLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 
+import java.io.Serial;
+
 
 public class LoggableDispatcherServlet extends DispatcherServlet {
+    @Serial
     private static final long serialVersionUID = -8650983209144626130L;
 
     @Override
-    protected void doDispatch(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    protected void doDispatch(HttpServletRequest request, HttpServletResponse response) {
         if (!(request instanceof ContentCachingRequestWrapper)) {
             request = new ContentCachingRequestWrapper(request);
         }
-        super.doDispatch(request, response);
+        try {
+            super.doDispatch(request, response);
+        } catch (Exception e) {
+            UserManagerLogger.errorMessage(this.getClass(), e);
+        }
     }
 }
